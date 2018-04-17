346da47e2bdd1b0662812b3fc3144df602319fa4
Spencer Gardner <spencergardner@gmail.com>
2018-04-16 22:50:51 -0500

    bugfix

--- connected_census_blocks.py
+++ connected_census_blocks.py
@@ -85,7 +85,7 @@ cur.execute(" \
 
 conn.commit()
 
- cur.execute(" \
+cur.execute(" \
     CREATE UNIQUE INDEX idx_neighborhood_blockpairs ON neighborhood_connected_census_blocks (source_blockid10,target_blockid10); \
     CREATE INDEX IF NOT EXISTS idx_neighborhood_blockpairs_lstress ON neighborhood_connected_census_blocks (low_stress) WHERE low_stress IS TRUE; \
     CREATE INDEX IF NOT EXISTS idx_neighborhood_blockpairs_hstress ON neighborhood_connected_census_blocks (high_stress) WHERE high_stress IS TRUE; \
